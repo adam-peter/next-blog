6362dc34c905823eeecbe38760bd95c064fba907
badmood111 <petrzela.adam@seznam.cz>
2023-02-13 18:18:45 +0100

    Configure styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import "../styles/globals.css"
+import "../styles/globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }): JSX.Element {
   return (
     <html lang="en">
       <head />
-      <body>
+      <body className="bg-black text-white w-8/12 mx-auto text-lg">
+        <div className="">Layout</div>
         {children}
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
+"use client";
+/* eslint-disable react/no-unescaped-entities */
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-[100vh] w-[100vw] bg-black text-white flex justify-center items-center ">
-      <div>
+    <div className="">
+      <div className="shadow">
         <div>
-          <h1 className="">my name is Adam Petržela</h1>
+          <h1 className="text-4xl font-bold">
+            Hi, I'm{" "}
+            <a
+              href="https://github.com/badmood111"
+              target="_blank"
+              className="text-blue-500 hover:text-purple-500" rel="noreferrer"
+            >
+              @badmood111
+            </a>
+          </h1>
         </div>
         <div>
-          <Link href="/blog">checkout my Blog</Link>
+          <Link href="/blog" className="text-xl hover:text-purple-500">
+            checkout my Blog
+          </Link>
         </div>
         <div>
-          <Link href="/contact">contact me</Link>
+          <Link href="/contact" className="text-xl hover:text-purple-500">contact me</Link>
         </div>
       </div>
     </div>
