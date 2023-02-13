2e8703e9aaa1b16e5242feded4e992b07fe0ccec
badmood111 <petrzela.adam@seznam.cz>
2023-02-13 17:32:58 +0100

    root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div className="bg-blue-400">Hello!</div>
+    <div className="h-[100vh] w-[100vw] bg-black text-white flex justify-center items-center ">
+      <div>
+        <div>
+          <h1 className="">my name is Adam Petržela</h1>
+        </div>
+        <div>
+          <Link href="/blog">checkout my Blog</Link>
+        </div>
+        <div>
+          <Link href="/contact">contact me</Link>
+        </div>
+      </div>
     </div>
   );
 }
